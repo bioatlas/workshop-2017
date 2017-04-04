f326366dea84028a94a47c1f4cd5f3433fc206e0
Markus Skyttner <markus.skyttner@nrm.se>
2017-04-04 13:25:19 +0200

    add index page

---
+++ get_image.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p fig
+wget https://upload.wikimedia.org/wikipedia/commons/3/35/Stockholm_city_modified.jpg -O fig/stockholm.jpg
+wget https://bioatlas.se/content/images/2017/03/GBIF-2015-mark.jpg -O fig/gbif-logo.jpg
